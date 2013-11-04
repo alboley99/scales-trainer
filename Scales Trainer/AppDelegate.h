e5bd0cb9eaf26d1cffd21bfe247f54227a244d3d
Alec Hoffman <alecjhoffman@gmail.com>
2013-11-04 08:59:35 +0000

    Initial Commit

---
+++ Scales Trainer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Scales Trainer
+//
+//  Created by Alec Hoffman on 04/11/2013.
+//  Copyright (c) 2013 Alec Hoffman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
